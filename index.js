a3b5383204d25b50d039a39e9ad2b4f78873f7eb
Alejandro López <a.lopez.sanchez@outlook.es>
2018-03-25 22:05:42 +0200

    Add prettier configuration
    Add package-lock.json and WIP ejs templating

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const figlet = require("figlet");
 
 const inquirer = require("./lib/inquirer");
 const npm = require("./lib/npm");
+const templates = require("./lib/templates");
 
 clear();
-console.info(
-  chalk.yellow(
-    figlet.textSync("Web tooling benchmark")
-  )
-);
+console.info(chalk.yellow(figlet.textSync("Web tooling benchmark")));
 
 const run = async () => {
   const answers = await inquirer.ask();
 
   npm.install(answers.library);
+
+  await templates.compileLicense();
 };
 
 run();
