d59e1a2d960622bf8bc814b7e8d0362570957665
Alejandro López <a.lopez.sanchez@outlook.es>
2018-03-28 00:37:57 +0200

    add checks and end files creation

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
+#!/usr/bin/env node
+
 const clear = require("clear");
 const chalk = require("chalk");
 const figlet = require("figlet");
+const { EOL } = require("os");
+
+const config = require("./config");
 
 const inquirer = require("./lib/inquirer");
-const npm = require("./lib/npm");
-const templates = require("./lib/templates");
+// const npm = require("./lib/npm");
+const sourceFiles = require("./lib/source-files");
+const checks = require("./lib/checks");
 
 clear();
 console.info(chalk.yellow(figlet.textSync("Web tooling benchmark")));
 
 const run = async () => {
-  const answers = await inquirer.ask();
+  if (!checks.isWebToolingBenchmark()) {
+    console.error("It seems that you are not in the correct repository.");
+    console.error(`Please, go to your ${config.repositoryName} clone${EOL}`);
+    process.exit(0);
+  }
 
-  npm.install(answers.library);
+  const { library } = await inquirer.ask();
 
-  await templates.compileLicense();
+  if (checks.isAlreadyABenchmark(library)) {
+    console.error(
+      "It seems that there is already a benchmark for this library"
+    );
+    process.exit(0);
+  }
+
+  // npm.install(answers.library);
+
+  await sourceFiles.createBenchmarkFile(library);
+  await sourceFiles.createBenchmarkTestFile(library);
 };
 
 run();
