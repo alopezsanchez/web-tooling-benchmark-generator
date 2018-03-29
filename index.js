f98e48e4a6ed86813abc17301e33feae3c01c43c
Alejandro López <a.lopez.sanchez@outlook.es>
2018-03-29 14:32:48 +0200

    WIP new CLI
    Fix require problems

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const figlet = require("figlet");
 
 const config = require("./config");
 
-const cli = require("./lib/cli");
-const logger = require("./lib/logger");
+const CLI = require("./lib/cli");
+const { SUCCESS, ERROR, WARN, INFO } = require("./lib/cli/status");
 const npm = require("./lib/npm");
 const sourceFiles = require("./lib/source-files");
 const checks = require("./lib/checks");
@@ -20,27 +20,61 @@ clear();
 console.info(chalk.yellow(figlet.textSync("Web tooling benchmark")));
 
 const run = async () => {
+  const cli = new CLI();
+
+  /* ************************************* */
+
+  const checkRepoMessage = `Checking if you are in a ${
+    config.repositoryName
+  } benchmark repository...`;
+
+  cli.startSpinner(checkRepoMessage);
   if (!checks.isWebToolingBenchmark()) {
-    logger.error("It seems that you are not in the correct repository.");
-    logger.error(`Please, go to your ${config.repositoryName} clone.${EOL}`);
-    process.exit(1);
-  }
-
-  const { library } = await cli.ask();
-
-  if (checks.isAlreadyABenchmark(library)) {
-    logger.error(
-      "It seems that there is already a benchmark for this library."
+    cli.persist(
+      "It seems that you are not in the correct repository. " +
+        `Please, go to your ${config.repositoryName} clone.${EOL}`,
+      ERROR
     );
     process.exit(1);
   }
+  cli.persist(checkRepoMessage, SUCCESS);
 
-  await npm.install(library);
+  /* ************************************* */
+
+  const { library } = await cli.ask();
+
+  /* ************************************* */
+
+  const checkBenchmarkMessage =
+    "Checking if a benchmark for this library doesn't exist...";
+
+  cli.startSpinner(checkBenchmarkMessage);
+  if (checks.isAlreadyABenchmark(library)) {
+    cli.persist(
+      "It seems that there is already a benchmark for this library.",
+      ERROR
+    );
+    process.exit(1);
+  }
+  cli.persist(checkBenchmarkMessage, SUCCESS);
+
+  /* ************************************* */
+
+  npm.install(library, cli);
+
+  /* ************************************* */
 
   await sourceFiles.createBenchmarkFile(library);
+
+  /* ************************************* */
+
   await sourceFiles.createBenchmarkTestFile(library);
 
+  /* ************************************* */
+
   await docs.createNewSection(library);
+
+  /* ************************************* */
 
   await targetList.addLibrary(library);
 };
